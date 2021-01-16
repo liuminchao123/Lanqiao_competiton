955cea78b1ae5b135de4d689aac77dca2590ed6f
liuminchao123 <mr.liuminchao@outlook.com>
2021-01-16 22:00:05 +0800

    第一周作业

---
+++ 第一周作业/t4.java
@@ -0,0 +1,55 @@
+
+class Main{
+	
+	public static void main(String[] args) {
+		dfs(0);
+		System.out.println(ans);
+	}
+	
+	static int[] a = new int[] {1,2,3,4,5,6,7,8,9};
+	static int ans=0; // 计数器
+
+
+	static void dfs(int m) {
+		if(m>=9) {
+			// 给abcdefghi 赋不同的值
+			int A = a[0];
+			int B = a[1];
+			int C = a[2];
+			int D = 100*a[3]+10*a[4]+a[5];
+			int E = 100*a[6]+10*a[7]+a[8];
+
+			// for(int i=0;i<9;i++){
+			// 	System.out.println("m = " + m);
+			// 	System.out.print("a[" + i + "] = " + a[i]+" "); // 打印出使等式成立的情况
+			// 	System.out.println();
+			// }
+ 
+			if(A*C*E + B*E +D*C == 10*C*E) {
+				ans++;  // 计数器加一
+				for(int i=0;i<9;i++){
+					System.out.print(a[i]+" "); // 打印出使等式成立的情况
+				}
+				System.out.println();
+			}
+
+			return; // 终止函数，返回空值
+		}
+		
+		// 
+		for(int i = m; i < 9; i ++) {
+			swap(m,i);
+			dfs(m+1);
+			swap(m,i);
+		}
+		
+	}
+ 
+	static void swap(int i, int j) {
+		int t = a[i];
+		a[i] = a[j];
+		a[j] = t;
+	}
+
+
+}
